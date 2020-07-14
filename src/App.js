a96f9995ee4e72a8ce9874e8e7edc0d843222c67
조윤식 <joyoonsik@joyunsig-ui-MacBookPro.local>
2020-07-14 17:33:58 +0900

    again code upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX:"auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
+
+const customers = [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'조윤식',
+  'birthday':'990507',
+  'gender':'남자',
+  'job':'대학생',
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'이재표',
+  'birthday':'990111',
+  'gender':'남자',
+  'job':'육군',
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'장준영',
+  'birthday':'990222',
+  'gender':'남자',
+  'job':'해병대',
+}
+]
+
+class App extends Component{
+  render() {
+    const {classes}=this.props;
+    return(
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c=>{return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+          </TableBody>
+        </Table>
+        
+      </Paper> 
+    );
+  }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
